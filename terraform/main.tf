5dcb52dccd1f0a678f9e2f1d1ba3ed0dd0386aa6
gdeoliveiradata <gdeoliveiradata@gmail.com>
2026-03-10 19:57:49 -0300

    Initial configuration and setup of Terraform with remote backend on a GCS bucket.

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.23.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "is-rock-alive-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.PROJECT_ID
+  region  = var.PROJECT_REGION
+  zone    = var.PROJECT_ZONE
+}
